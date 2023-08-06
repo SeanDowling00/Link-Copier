8a929c6fda2c1c53cbe312c7fc8c3135a0b2d516
SeanDowling00 <72495332+SeanDowling00@users.noreply.github.com>
2023-08-06 08:43:48 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,33 @@
+chrome.tabs.query({ currentWindow: true }, function(tabs) {
+  var tabLinksList = document.getElementById('tabLinks');
+  var copyButton = document.getElementById('copyButton');
+
+ 
+  filteredTabs.forEach(function(tab) {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.textContent = tab.title;
+    a.href = tab.url;
+    a.target = '_blank';
+    li.appendChild(a);
+    tabLinksList.appendChild(li);
+  });
+
+  copyButton.addEventListener('click', function() {
+    var linkText = filteredTabs.map(function(tab) {
+      return tab.title + ': ' + tab.url;
+    }).join('\n');
+
+    var tempInput = document.createElement('textarea');
+    tempInput.value = linkText;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    copyButton.textContent = 'Copied!';
+    setTimeout(function() {
+      copyButton.textContent = 'Copy All Links to Clipboard';
+    }, 2000);
+  });
+});
