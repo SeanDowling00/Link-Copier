63780bda2ddfbede8ddab6f37be14578cbabedd7
SeanDowling00 <72495332+SeanDowling00@users.noreply.github.com>
2023-08-06 08:46:51 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
-chrome.tabs.query({ currentWindow: true }, function(tabs) {
+chrome.tabs.query({}, function(tabs) {
   var tabLinksList = document.getElementById('tabLinks');
   var copyButton = document.getElementById('copyButton');
 
- 
-  filteredTabs.forEach(function(tab) {
+  tabs.forEach(function(tab) {
     var li = document.createElement('li');
     var a = document.createElement('a');
     a.textContent = tab.title;
@@ -14,7 +13,39 @@ chrome.tabs.query({ currentWindow: true }, function(tabs) {
   });
 
   copyButton.addEventListener('click', function() {
-    var linkText = filteredTabs.map(function(tab) {
+    var linkText = tabs.map(function(tab) {
+      return tab.title + ': ' + tab.url;
+    }).join('\n');
+
+    var tempInput = document.createElement('textarea');
+    tempInput.value = linkText;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    copyButton.textContent = 'Copied!';
+    setTimeout(function() {
+      copyButton.textContent = 'Copy All Links to Clipboard';
+    }, 2000);
+  });
+});
+chrome.tabs.query({}, function(tabs) {
+  var tabLinksList = document.getElementById('tabLinks');
+  var copyButton = document.getElementById('copyButton');
+
+  tabs.forEach(function(tab) {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.textContent = tab.title;
+    a.href = tab.url;
+    a.target = '_blank';
+    li.appendChild(a);
+    tabLinksList.appendChild(li);
+  });
+
+  copyButton.addEventListener('click', function() {
+    var linkText = tabs.map(function(tab) {
       return tab.title + ': ' + tab.url;
     }).join('\n');
 
